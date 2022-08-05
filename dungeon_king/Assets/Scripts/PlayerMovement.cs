8777e5742fadbe3f0641db257c45111992b532c9
senseji <34347036+senseji@users.noreply.github.com>
2022-08-05 13:22:51 +0200

    add shooting (#5)
    * add shooting
    
    * delete 5 fireballs

--- dungeon_king/Assets/Scripts/PlayerMovement.cs
+++ dungeon_king/Assets/Scripts/PlayerMovement.cs
@@ -79,15 +79,6 @@ public class PlayerMovement : MonoBehaviour
             wallJumpCooldown = 0;
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-    }
-
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
@@ -98,5 +89,10 @@ public class PlayerMovement : MonoBehaviour
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall();
     }
 }
