ae77a6db57d4f484890211769c58e4579951d60f
senseji <lovrirobert7@gmail.com>
2022-08-06 14:36:39 +0200

    add camera movement

---
+++ dungeon_king/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    // Room Camera
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+
+    // Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    private void Awake()
+    {
+        currentPosX = transform.position.x;
+    }
+
+
+    private void Update()
+    {
+        // Room Camera
+        // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        
+
+        // Follow Player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, aheadDistance * player.localScale.x, cameraSpeed * Time.deltaTime);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
