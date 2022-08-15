c00fcac7caa25fbbcb12c0d16957c6945378d8e4
senseji <lovrirobert7@gmail.com>
2022-08-15 19:29:33 +0200

    add enemies

--- dungeon_king/Assets/Scripts/Health/Health.cs
+++ dungeon_king/Assets/Scripts/Health/Health.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+
 public class Health : MonoBehaviour
 {
     [Header ("Health")]
@@ -10,8 +11,12 @@ public class Health : MonoBehaviour
 
     [Header("iFrames")]
     [SerializeField] private float iFramesDuration;
-    [SerializeField] private float numberOfFlashes;
+    [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
+
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
 
     private void Awake()
     {
@@ -21,20 +26,24 @@ public class Health : MonoBehaviour
     }
     public void TakeDamage(float _damage)
     {
+        if (invulnerable) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            //iframes
-            StartCoroutine(Invulnerability());
+            StartCoroutine(Invunerability());
         }
         else
         {
             if (!dead)
             {
                 anim.SetTrigger("die");
-                GetComponent<PlayerMovement>().enabled = false;
+
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
+
                 dead = true;
             }
         }
@@ -43,12 +52,11 @@ public class Health : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
-
-    private IEnumerator Invulnerability()
+    private IEnumerator Invunerability()
     {
+        invulnerable = true;
         Physics2D.IgnoreLayerCollision(10, 11, true);
-
-        for(int i = 0; i < numberOfFlashes; i++)
+        for (int i = 0; i < numberOfFlashes; i++)
         {
             spriteRend.color = new Color(1, 0, 0, 0.5f);
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
@@ -56,5 +64,11 @@ public class Health : MonoBehaviour
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
         Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
